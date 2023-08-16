a786041f6be8ac5f57909daf0dcd84e9ba395e4d
Blag <atejada@gmail.com>
2023-08-16 08:43:42 -0400

    Code for V3

--- ReadEvents.rb
+++ ReadEvents.rb
@@ -1,44 +1,47 @@
+# Load gems
 require 'dotenv/load'
+require 'nylas/api'
 require 'nylas'
-require 'date'
 
-nylas = Nylas::API.new(
-    app_id: ENV["CLIENT_ID"],
-    app_secret: ENV["CLIENT_SECRET"],
-    access_token: ENV["ACCESS_TOKEN"]
+# Initialize Nylas client
+nylas = Nylas::Client.new(
+	api_key: ENV["V3_TOKEN"]
 )
 
-today = Date.today
-_START = Time.local(today.year, today.month, today.day, 0, 0, 0).strftime("%s")
-_END = Time.local(today.year, today.month, today.day, 23, 59, 59).strftime("%s")
+# Query parameters
+query_params = {
+    calendar_id: ENV["GRANT_ID"]
+  }
 
-events = nylas.events.where(calendar_id: ENV["CALENDAR_ID"],starts_after: _START,ends_before: _END)
-                                                               
-events.each{ |event|
-test = true
-	if event.when.start_time
-		print(
-			"Id: #{event.id} | " \
-			"Title: #{event.title} | " \
-			"Start: #{event.when.start_time} | " \
-			"End: #{event.when.end_time} | " \
-			"Participants: ["
-			)
-	else
-		print(
-			"Id: #{event.id} | " \
-			"Title: #{event.title} | " \
-			"Date: #{event.when.date} | " \
-			"Participants: ["
-			)	
-	end		
-        event.participants.each{ |participant|
-			print (
-				"{comment: #{participant.comment}, " \
-				"email: #{participant.email}, " \
-				"name: #{participant.name}, " \
-				"status: #{participant.status}}"
-			)
-        }
-        puts("]\n\n")
+query_params['start'] = "1692187200" # From 8:00am
+query_params['end'] = "1692219600" # To 5:00pm
+
+path_params = {
+    grant_id: ENV["GRANT_ID"]
+  }
+
+# Read events from our main calendar in the specified date and time
+events, _request_ids = nylas.events.list(path_params: path_params, query_params: query_params)
+
+# Loop events
+events[:data].each {|event|
+	case event[:when][:object]
+		when 'timespan'
+			start_time = Time.at(event[:when][:start_time])
+			end_time = Time.at(event[:when][:end_time])
+			event_date = "The time of the event is from: #{start_time} to #{end_time}"
+		when 'datespan'
+			start_time = event[:when][:start_date]
+			end_time = event[:when][:end_date]
+			event_date = "The date of the event is from: #{start_time} to: #{end_time}"
+		when 'date'
+			start_time = event[:when][:date]
+			event_date = "The date of the event is: #{start_time} "
+		end
+		participant_details = ""
+		event[:participants].each {|participant|
+			participant_details = "Email: #{participant[:email]} Name: #{participant[:email]} Status: #{participant[:status]}"
+		}
+		puts "Id: #{event[:id]} | Title: #{event[:title]} | #{event_date} | Participants: #{participant_details}"
+		puts "\n"
 }
